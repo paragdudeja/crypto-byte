1b0c9bef20e880c30aae66607b07be639fa355ba
Parag Dudeja <parag1907@gmail.com>
2022-06-09 03:49:40 +0530

    First commit

---
+++ src/Components/SelectButton.js
@@ -0,0 +1,38 @@
+import { makeStyles } from '@material-ui/core'
+import React from 'react'
+
+const SelectButton = ({ children, selected, onClick }) => {
+
+	const useStyles = makeStyles(() => ({
+		selectButton: {
+			border: "1px solid gold",
+			borderRadius: 5,
+			padding: 10,
+			paddingLeft: 10,
+			paddingRight: 20,
+			fontFamily: "Montserrat",
+			cursor: "pointer",
+			backgroundColor: selected ? "gold" : "",
+			color: selected ? "black" : "",
+			fontWeight: selected ? 700 : 500,
+			"&:hover": {
+				backgroundColor: "gold",
+				color: "black",
+			},
+			width: "22%",
+		}
+	}));
+
+	const classes = useStyles();
+
+  return (
+    <span
+			className={classes.selectButton}
+			onClick={onClick}
+		>
+      { children }
+    </span>
+  )
+}
+
+export default SelectButton
